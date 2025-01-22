1953034463d9e537488039c5b76fc884d52efb7d
Roderick Shoolbraid <rdxenv@gmail.com>
2025-01-22 09:59:52 -0800

    username - optional

--- db/users.sql
+++ db/users.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users CASCADE;
 -- Create the users table
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,                           -- Unique identifier for each user
-    username VARCHAR(100) NOT NULL,                 -- Username (required)
+    username VARCHAR(100),                          -- Username (optional)
     email VARCHAR(100) UNIQUE NOT NULL,             -- Email address (unique and required)
     password VARCHAR(255) NOT NULL,                 -- Encrypted password (required)
     terms_agreed BOOLEAN NOT NULL DEFAULT FALSE,    -- Terms and conditions agreement (default false)
@@ -14,5 +14,5 @@ CREATE TABLE users (
 -- Insert sample data
 INSERT INTO users (username, email, password, terms_agreed)
 VALUES 
-    ('jdoe', 'jdoe@example.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', TRUE),
-    ('asmith', 'asmith@example.com', '$2a$10$IgMNeWXzv1vD3V7mQjQpquasMUAay9vQBIS6CSeoT6/HixxY7rsTi', FALSE);
+    (NULL, 'jdoe@example.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', TRUE),
+    (NULL, 'asmith@example.com', '$2a$10$IgMNeWXzv1vD3V7mQjQpquasMUAay9vQBIS6CSeoT6/HixxY7rsTi', FALSE);
